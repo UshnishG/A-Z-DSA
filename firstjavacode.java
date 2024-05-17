14e872b2164b47fdeadfcb1fc79cb135f82c82d4
Ushnish <ghosalushnish@gmail.com>
2024-05-17 21:17:49 +0530

    1st java code testing

---
+++ firstjavacode.java
@@ -0,0 +1,8 @@
+public class firstjavacode {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int sum = num1 + num2;
+        System.out.println("The sum is: " + sum);
+    }
+}
