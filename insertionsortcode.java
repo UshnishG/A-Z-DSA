c230be6ba90f6b7a3254eba94d123821a5284de7
Ushnish Ghosal <ghosalushnish@gmail.com>
2024-06-17 15:07:08 +0530

    Add files via upload

--- insertionsortcode.java
+++ insertionsortcode.java
@@ -1,14 +1,23 @@
 public class insertionsortcode {
 
     public static void insertionsort(int arr[]){
-        
+       int n = arr.length;
+       for (int i = 1; i < n; i++) {
+           int curr = arr[i];
+           int prev = i - 1;
+           while (prev >= 0 && arr[prev] > curr) {
+               arr[prev + 1] = arr[prev];
+               prev--;
+           }
+           arr[prev + 1] = curr;
+       }
     }
     
-
-
-
-
-
+    public static void printarr(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]);
+        }
+    }
 
     public static void main(String[] args) {
         int arr[] = {2,3,4,5,6,7,8,1};
